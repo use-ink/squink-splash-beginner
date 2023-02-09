197cc35f9e096ef585f1b30fb6c5d7193ec7dc09
Leandro <leapalazzolo@gmail.com>
2023-02-08 23:26:00 -0300

    feat: update docs and contract

--- lib.rs
+++ lib.rs
@@ -3,30 +3,36 @@
 #[ink::contract]
 mod player {
     #[ink(storage)]
-    pub struct Player {}
-
+    pub struct Player {
+        x: u32,
+        y: u32,
+    }
+    #[ink(event)]
+    pub struct NewPoint {
+        x: u32,
+        y: u32,
+    }
     impl Player {
         #[ink(constructor)]
         pub fn new() -> Self {
-            Self {}
+            Self { x: 0, y: 0 }
         }
-
-        /// This function will be called during every game round.
-        ///
-        /// The function returns a `Some((x, y))` coordinate of the pixel which you
-        /// want to color.
-        ///
-        /// # Notes
-        ///
-        /// The function signature `&mut self` is so that you can retain state
-        /// in the contract's storage if you want to.
-        ///
-        /// The function can be named as you like, but it always needs to have
-        /// a defined selector of `0`.
         #[ink(message, selector = 0)]
-        pub fn your_turn(&self) -> Option<(u32, u32)> {
-            // TODO: return the turn you want to make (x, y)
-            Some((0, 0))
+        pub fn your_turn(&mut self) -> Option<(u32, u32)> {
+            let old_x = self.x;
+            let old_y = self.y;
+            if self.x == 25 {
+                self.x = 0;
+                if self.y == 25 {
+                    self.y = 0;
+                } else {
+                    self.y += 1;
+                }
+            } else {
+                self.x += 1;
+            }
+            self.env().emit_event(NewPoint { x: old_x, y: old_y });
+            Some((old_x, old_y))
         }
     }
 }
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Ink! Workshop
+
+The idea of the [workshop](https://www.eventbrite.com/e/workshop-de-ink-h-bulgarini-r-rius-y-p-corrado-polkadot-side-event-3-tickets-532332459717?utm_source=eventbrite&utm_medium=email&utm_campaign=reminder_attendees_48hour_email&utm_term=eventname&ref=eemaileventremind) is to create a smart contract using ink! and register it in this contract 5EVWXZPkNH3UogY2WZr4ubxRPFDcQ3q2ikusi2aY5gJnfWVd. 
+
+I deployed my custom contract [lib.rs](lib.rs) and registered it using the contract method `registerPlayer`. 
+
+BTW, I (leandro) won the game https://splash.use.ink/game/5EVWXZPkNH3UogY2WZr4ubxRPFDcQ3q2ikusi2aY5gJnfWVd!
+
+![.images/winner.png](.images/winner.png)
+
+# Install
+
+Read here [todo-1.md](todo-1.md)
+
+# Deploy and register the contract
+
+Read here [todo-2.md](todo-2.md)
